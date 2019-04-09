d8914f5b81987b832d487f940215edb1b4fdb805
amanpandey104 <amanpandey1504@gmail.com>
2019-04-09 19:46:45 +0530

    Update send_frame&data_receive_controller_.py

--- send_frame&data_receive_controller_.py
+++ send_frame&data_receive_controller_.py
@@ -63,11 +63,12 @@ def receive_controller_data():
         time.sleep(.01)
 
 
-def send_sensor_values(temp, ph):    
-    list1 = [temp, ph]
-    data = pickle.dumps(list1)
-    conn.send(data)
-    time.sleep(.01)
+def send_sensor_values(temp, ph):
+    while True:
+        list1 = [temp, ph]
+        data = pickle.dumps(list1)
+        conn.send(data)
+        time.sleep(.01)
 
 '''
 def send_frame():
@@ -92,7 +93,7 @@ def send_frame():
 '''    
     
 recv_cont = threading.Thread(target = receive_controller_data, args = ())
-#send_sense = threading.Thread(target = arduino, args = (''))
+send_sense = threading.Thread(target = send_sensor_values, args = ('',''))
 #send_cam = threading.Thread(target = send_frame, args = ())
 
 recv_cont.start()
