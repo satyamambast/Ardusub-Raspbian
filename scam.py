a77edd4ff1226bd1fc17d8f69af5e71153848d7d
satyamambast <36452790+satyamambast@users.noreply.github.com>
2019-04-17 23:34:16 +0530

    Update scam.py
    added camera feed

--- scam.py
+++ scam.py
@@ -6,7 +6,32 @@ import struct
 import io
 import cv2
 import serial
+class MultiCam:
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    def __init__(self):
+        self.frame1=None
+        self.frame2=None
+        self.frame3=None
+        self.frame4=None
+    def encodepossible(self,cam1,cam2,cam3,cam4):
+        self.ret1, frame1 = cam1.read()
+        self.ret2, frame2 = cam2.read()
+        self.ret3, frame3 = cam3.read()
+        self.ret4, frame4 = cam4.read()
+        if ret1:
+            result, self.frame1 = cv2.imencode('.jpg', frame1, encode_param)            
+        elif ret2:
+            result, self.frame2 = cv2.imencode('.jpg', frame2, encode_param)            
+        elif ret3:
+            result, self.frame3 = cv2.imencode('.jpg', frame3, encode_param)            
+        elif ret3:
+            result, self.frame4 = cv2.imencode('.jpg', frame4, encode_param)            
 
+    def displayallfeeds(self):
+	cv2.imshow('cam1',self.frame1)
+	cv2.imshow('cam2',self.frame2)
+	cv2.imshow('cam3',self.frame3)
+	cv2.imshow('cam4',self.frame4)
 host = ('192.168.2.2',5058)
 global conn,addr,k
 msg1=[]
@@ -73,27 +98,38 @@ def send_sensor_values():
         conn.send(data)
         time.sleep(.01)
 
-'''
 def send_frame():
     client_socket = socket.socket(socket.AF_INET, sock.SOCK_STREAM)
     client_socket.connect(('192.168.2.1', 5003))
 
-    cam = cv2.VideoCapture(1)
-    cam.seet(3,320)
-    cam.set(4, 240)
+    cam1 = cv2.VideoCapture(0)
+    cam1.set(3,320)
+    cam1.set(4, 240)
+    cam2 = cv2.VideoCapture(1)
+    cam2.set(3,320)
+    cam2.set(4, 240)
+    cam3 = cv2.VideoCapture(2)
+    cam3.set(3,320)
+    cam3.set(4, 240)
+    cam4 = cv2.VideoCapture(3)
+    cam4.set(3,320)
+    cam4.set(4, 240)
+
     img_counter = 0
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    
     
     while True:
-        ret, frame = cam.read()
-        result, frame = cv2.inencode('.jpg', frame, encode_param)
+        obj=MultiCam()
+        obj.encodepossible(cam1,cam2,cam3,cam4)
+        #ret, frame = cam.read()
+        #result, frame = cv2.inencode('.jpg', frame, encode_param)
+        data=pickle.dumps(obj,1)
         size = len(data)
 
         print("{}: {}".format(img_counter, size))
         client_socket.sendall(struct.pack(">L",size) + data)
         img_counter += 1
     cam.release()
-'''    
     
 recv_cont = threading.Thread(target = receive_controller_data, args = ())
 send_sense = threading.Thread(target = send_sensor_values, args = ())
